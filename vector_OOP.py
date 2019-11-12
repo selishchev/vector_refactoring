8f1cd8b03390076fc09d8f5a17ad5ae81b87a894
Alexandr Selishchev <44421225+selishchev@users.noreply.github.com>
2019-11-12 22:03:36 +0700

    Add files via upload

--- vector_OOP.py
+++ vector_OOP.py
@@ -10,26 +10,32 @@ class Vector:
     def __init__(self, x):
         self.x = x
 
-    def __sub__(self, vec):  # разность векторов
-        return self.x[0] - vec.x[0], self.x[1] - vec.x[1]
+    def __sub__(self, other):  # разность векторов
+        return self.x[0] - other.x[0], self.x[1] - other.x[1]
 
-    def __add__(self, vec):  # сумма векторов
-        return self.x[0] + vec.x[0], self.x[1] + vec.x[1]
+    def __add__(self, other):  # сумма векторов
+        return self.x[0] + other.x[0], self.x[1] + other.x[1]
 
     def __len__(self):  # длинна вектора
         return sqrt(self.x[0] * self.x[0] + self.x[1] * self.x[1])
 
-    def __mul__(self, k):
-        return self.x[0] * k, self.x[1] * k
+    def __mul__(self, other):
+        if self.x != int or self.x != float:
+            return self.x[0] * other, self.x[1] * other
+        else:
+            return self.x[0] * other.x[0], self.x[1] * other.x[1]
 
     def int_pair(self):
-        return self.x[1], self.x[0]
+        return int(self.x[0]), int(self.x[1])
 
 
 class Line:
 
-    @staticmethod
-    def draw_points(points, style="points", width=4, color=(255, 255, 255)):
+    def __init__(self):
+        self.speeds = []
+        self.points = []
+
+    def draw_points(self, points, style="points", width=4, color=(255, 255, 255)):
         if style == "line":
             for point_number in range(-1, len(points) - 1):
                 pygame.draw.line(gameDisplay, color, (int(points[point_number][0]), int(points[point_number][1])),
@@ -54,8 +60,7 @@ class Line:
             result.append(self.get_point(base_points, i * alpha))
         return result
 
-    @staticmethod
-    def set_points(points, speeds):
+    def set_points(self, points, speeds):
         for point in range(len(points)):
             points[point] = Vector(points[point]) + Vector(speeds[point])
             if points[point][0] > SCREEN_SIZE[0] or points[point][0] < 0:
@@ -63,13 +68,11 @@ class Line:
             if points[point][1] > SCREEN_SIZE[1] or points[point][1] < 0:
                 speeds[point] = (speeds[point][0], -speeds[point][1])
 
-    @staticmethod
-    def delete_point(index):
+    def delete_point(self, index):
         points.pop(index)
         speeds.pop(index)
 
-    @staticmethod
-    def change_speed(n):
+    def change_speed(self, n):
         for i in range(len(speeds)):
             if -20 < speeds[i][0] < 20 and -20 < speeds[i][1] < 20:
                 speeds[i] = Vector(speeds[i]) * n
@@ -80,12 +83,10 @@ class Line:
 
 class Joint(Line):
 
-    def __init__(self, points, count):
+    def __init__(self):
         super().__init__()
-        self.count = count
-        self.points = points
 
-    def get_joint(self):
+    def get_joint(self, count):
         if len(points) < 3:
             return []
         result = []
@@ -95,7 +96,7 @@ class Joint(Line):
             pnt.append(points[i + 1])
             pnt.append(Vector((Vector(points[i + 1]) + Vector(points[i + 2]))) * 0.5)
 
-            result.extend(self.get_points(pnt, self.count))
+            result.extend(self.get_points(pnt, count))
         return result
 
 
@@ -135,6 +136,7 @@ if __name__ == "__main__":
     speeds = []
     show_help = False
     pause = False
+    joint = Joint()
 
     color_param = 0
     color = pygame.Color(0)
@@ -159,11 +161,11 @@ if __name__ == "__main__":
                     steps -= 1 if steps > 1 else 0
                 if event.key == pygame.K_DELETE:
                     if len(points) > 0:
-                        Line.delete_point(-1)
+                        joint.delete_point(-1)
                 if event.key == pygame.K_UP:
-                    Line.change_speed(2)
+                    joint.change_speed(2)
                 if event.key == pygame.K_DOWN:
-                    Line.change_speed(0.5)
+                    joint.change_speed(0.5)
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 points.append(event.pos)
@@ -172,10 +174,10 @@ if __name__ == "__main__":
         gameDisplay.fill((0, 0, 0))
         color_param = (color_param + 1) % 360
         color.hsla = (color_param, 100, 50, 100)
-        Line.draw_points(points)
-        Line.draw_points(Joint(points, steps).get_joint(), "line", 4, color)
+        joint.draw_points(points)
+        joint.draw_points(joint.get_joint(steps), "line", 4, color)
         if not pause:
-            Line.set_points(points, speeds)
+            joint.set_points(points, speeds)
         if show_help:
             display_help()
 
